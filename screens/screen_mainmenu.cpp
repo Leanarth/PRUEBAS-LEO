e83a5d09ca76d04e0cddc55dea0354b0c40df648
0therAnon <ba5892289@gmail.com>
2026-03-28 15:45:38 -0600

    Comentarios en screen_mainmenu.cpp

--- screens/screen_mainmenu.cpp
+++ screens/screen_mainmenu.cpp
@@ -4,63 +4,72 @@
 #include "../ui/input.hpp"
 #include "../ui/drawing.hpp"
 
-void screenMainmenuUpdate(Screen& currentScreen, bool& existstudent, std::string& studentName)
-{
-    cedulaBarPtr->status = isPressed(cedulaBarPtr);
-    if (cedulaBarPtr->status > 1)
-        inputfunc("backend", cedulaBarPtr, 21, "allchars", fontSize);
+// Esta función se encarga del backend de la pantalla MAINMENU
 
-    cedulaPtr->status = isPressed(cedulaPtr);
-    std::cout<<cedulaPtr->status<<"\n";
-    if ((IsKeyPressed(KEY_ENTER) || cedulaPtr->status == 4) &&
-        cedulaBarPtr->input == *admPassword)
+void screenMainmenuUpdate(Screen& currentScreen,          // Necesita la variable currentScreen para modificar la pantalla actual
+                          bool& existstudent,             // Neceista modificar la variable existstudent para indicar si el estudiante existe o no
+                          std::string& studentName)       // Necesita a la variable studentName para ingresar el nombre del estudiante si se encontró en la base de datos
+{
+    cedulaBarPtr->status = isPressed(cedulaBarPtr);                       // Se verifica el estado de la barra de datos de entrada de la cedula
+    if (cedulaBarPtr->status > 1)                                         // Si el estado de la barra es mayor a 1, significa que se quieren recibir datos de entrada, entonces...
+        inputfunc("backend", cedulaBarPtr, 21, "allchars", fontSize);     // Llama a inputfunc() para recibir datos de entrada
+
+    cedulaPtr->status = isPressed(cedulaPtr);                             // Se verifica el estado del botón cedula
+    if ((IsKeyPressed(KEY_ENTER) || cedulaPtr->status == 4) &&            // Si se presiona ENTER o se presiona el botón cedula, el cual se encuentra debajo de la barra de datos de entrada cedulaBar, y...
+        cedulaBarPtr->input == *admPassword)                              // el valor digitado en la barra de datos de entrada cedulaBar es igual a la contraseña del panel de ADMINMENU, entonces...
     {
-        currentScreen = ADMINMENU;
-        cedulaBarPtr->status = 0;
-        cedulaBarPtr->input  = "";
-        return;
+        cedulaPtr->status = 0;                            // El estado del botón cedula ahora será igual a cero
+        cedulaBarPtr->status = 0;                         // El estado de cedulaBar ahora será igual a cero para no recibir más datos de entrada
+        cedulaBarPtr->input  = "";                        // Y se vaciará la input
+        cedulaBarPtr->input32  = U"";                     // Y se vaciará la input
+        currentScreen = ADMINMENU;                        // La pantalla actual ahora será la del panel de administración
+        return;       // Retorno de la función, no se necesita hacer nada más
     }
 
-    if ((IsKeyPressed(KEY_ENTER) || cedulaPtr->status == 4) &&
-        !cedulaBarPtr->input.empty())
+    else if ((IsKeyPressed(KEY_ENTER) || cedulaPtr->status == 4) &&            // Si se presiona ENTER o se presiona el botón cedula, el cual se encuentra debajo de la barra de datos de entrada cedulaBar, y...
+        !cedulaBarPtr->input.empty())                                          // el valor digitado NO se encuentra vacío
     {
-        cedulaPtr->selfquery += cedulaBarPtr->input;
-        sendquery((cedulaPtr->selfquery + "'"s).data(), 1, 1);
+        cedulaPtr->status = 0;                                    // El estado del botón cedula ahora será igual a cero
+        cedulaBarPtr->status = 0;                                 // El estado de cedulaBar ahora será igual a cero para no recibir más datos de entrada
+        cedulaPtr->selfquery += cedulaBarPtr->input;              // Se procede a armar la query con el contenido de la barra cedulaBar para verificar si el estudiante existe en la base de datos
+        sendquery((cedulaPtr->selfquery + "'"s).data(), 1, 1);    // Se envía la query, recortando para que la output solo sea después de la primera columna, y antes de la última columna (por esos los "1,1" como argumentos en la función)
 
-        if (!outQuery.empty())
+        if (!outQuery.empty())                            // Si outQuery (la respuesta de la query) NO se encuentra vacía, entonces...
         {
-            existstudent = true;
-            studentName  = outQuery;
+            existstudent = true;                          // El estudiante SÍ existe
+            studentName  = outQuery;                      // Y el resultado de la query se guardará en studentName
         }
-        else
+        else                                              // En caso de que la respuesta de la query SÍ se encuentre vacía, entonces...
         {
-            existstudent = false;
-            regresarPtr->xloc = cedulaPtr->xloc;
+            existstudent = false;                         // Se declara que el estudiante NO existe
+            regresarPtr->xloc = cedulaPtr->xloc;          // Y el botón regresar se pondrá en el centro de la pantalla
         }
-        cedulaBarPtr->status = 0;
-        currentScreen = CONFIRMATION;
+        cedulaBarPtr->status = 0;                         // La barra cedulaBar no recibirá más datos de entrada y...
+        currentScreen = CONFIRMATION;                     // Se cambiará a la pantalla CONFIRMATION
     }
 }
 
+// Frontend de MAINMENU
+
 void screenMainmenuDraw()
 {
-    DrawTextEx(fontTtf, "Digite su cedula en este campo"s.data(),
+    DrawTextEx(fontTtf, "Digite su cedula en este campo"s.data(),                                                       // Se muestra un mensaje diciendo de que por favor digite la cédula en la barra
                (Vector2){(float)centertext("Digite su cedula en este campo"s, screenWidth, fontSize),
                           (float)(screenHeight * 0.23)},
                fontSize, 2, BLACK);
-    DrawTextEx(fontTtf, "con uno de los siguientes formatos"s.data(),
+    DrawTextEx(fontTtf, "con uno de los siguientes formatos"s.data(),                                                   // Se agrega en la pantalla el mensaje "con los siguientes formatos"
                (Vector2){(float)centertext("con uno de los siguientes formatos"s, screenWidth, fontSize),
                           (float)(screenHeight * 0.3)},
                fontSize, 2, BLACK);
-    DrawTextEx(fontTtf, "101110111 | 123456789012 | AZ0000-11111"s.data(),
+    DrawTextEx(fontTtf, "101110111 | 123456789012 | AZ0000-11111"s.data(),                                              // Y se agregan a la pantalla los tipos de formatos a ingresar
                (Vector2){(float)centertext("101110111 | 123456789012 | AZ0000-11111"s, screenWidth, fontSize),
                           (float)(screenHeight * 0.37)},
                fontSize, 2, BLACK);
-    PrettyDrawRectangle(cedulaBarPtr);
-    PrettyDrawRectangle(cedulaPtr);
+    PrettyDrawRectangle(cedulaBarPtr);                                                                                  // Se dibuja el rectángulo de la barra de datos de entrada de la cédula
+    PrettyDrawRectangle(cedulaPtr);                                                                                     // Y también se dibuja el botón de la cédula
     DrawTextEx(fontTtf, cedulaPtr->name.data(),
                (Vector2){cedulaPtr->xloc + (float)centertext(cedulaPtr->name, cedulaPtr->xsize, fontSize),
                           cedulaPtr->yloc + (float)((cedulaPtr->ysize - fontSize) / 2)},
                fontSize, 2, BLACK);
-    inputfunc("frontend", cedulaBarPtr, 0, "allchars", fontSize);
+    inputfunc("frontend", cedulaBarPtr, 0, "allchars", fontSize);                                                       // Se procede a dibujar el contenido de la barra de la cédula
 }
